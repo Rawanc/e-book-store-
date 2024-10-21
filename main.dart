e7bb00abfbf136d0de703976da635774e9aa31fd
BshayerFarhan <120379597+BshayerFarhan@users.noreply.github.com>
2024-10-21 23:34:10 +0300

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
+// ignore: depend_on_referenced_packages
+import 'package:provider/provider.dart';
 import 'package:flutter_application_1/pages/homepage.dart';
 import 'package:flutter_application_1/pages/productDetails.dart';
 import 'package:flutter_application_1/pages/profile.dart';
 import 'package:flutter_application_1/pages/setting.dart';
 import 'package:flutter_application_1/pages/shoppingCart.dart';
 import 'package:flutter_application_1/pages/signin.dart';
+import 'package:flutter_application_1/providers/settings_provider.dart';
 
 void main() {
-  runApp(const EbookStore());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => SettingsProvider(),
+      child: const EbookStore(),
+    ),
+  );
 }
 
 class EbookStore extends StatelessWidget {
@@ -15,17 +23,23 @@ class EbookStore extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.light(useMaterial3: true),
-      initialRoute: '/homepage',
-      routes: {
-        '/signin': (context) => const SignIn(),
-        '/homepage': (context) => const Homepage(),
-        '/setting': (context) => const Settings(),
-        '/profile': (context) => const Profile(),
-        '/shoppingcart': (context) => const ShoppingCart(),
-        '/productdetails': (context) => const ProductDetails(),
+    return Consumer<SettingsProvider>(
+      builder: (context, settings, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: settings.isDarkMode
+              ? ThemeData.dark(useMaterial3: true)
+              : ThemeData.light(useMaterial3: true),
+          initialRoute: '/homepage',
+          routes: {
+            '/signin': (context) => const SignIn(),
+            '/homepage': (context) => const Homepage(),
+            '/setting': (context) => const Settings(),
+            '/profile': (context) => Profile(),
+            '/shoppingcart': (context) => const ShoppingCart(),
+            '/productdetails': (context) => const ProductDetails(),
+          },
+        );
       },
     );
   }

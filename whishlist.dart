d2fa19ed28fa81261eb4b48c94efd3b642384040
Rawanc <159489893+Rawanc@users.noreply.github.com>
2024-10-24 07:33:33 +0300

    Update whishlist.dart

--- whishlist.dart
+++ whishlist.dart
@@ -1,9 +1,9 @@
-// wishlist.dart
 
+// wishlist.dart
 // ignore_for_file: use_super_parameters, library_private_types_in_public_api
 
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/pages/wishlisTItem.dart'; // Update the import path
+import 'package:flutter_application_1/pages/wishlistItem.dart'; // Update the import path
 
 class Wishlist extends StatefulWidget {
   final List<WishlistItemData> initialWishlist;
@@ -20,7 +20,14 @@ class _WishlistState extends State<Wishlist> {
   @override
   void initState() {
     super.initState();
-    _wishlistItems = widget.initialWishlist; // Initialize wishlist with passed data
+    // Initialize the wishlist with default data
+    _wishlistItems = [
+      WishlistItemData("assets/7.jpg", 'Harry Potter', 55.00),
+      WishlistItemData("assets/8.png", 'Murder on The Orient Express', 60.00),
+      WishlistItemData("assets/6.jpeg", 'A Crown Of Wishes', 75.00),
+    ];
+    // Add the items passed from the ProductDetails page (without overwriting the default ones)
+    _wishlistItems.addAll(widget.initialWishlist);
   }
 
   // Method to remove an item from the wishlist
@@ -30,23 +37,6 @@ class _WishlistState extends State<Wishlist> {
     });
   }
 
-  int _currentIndex = 0;
-
-  final List<WishlistItemData> _itemsToAdd = [
-    WishlistItemData("assets/7.jpg", 'Harry Potter', 55.00),
-    WishlistItemData("assets/8.png", 'Murder on The Orient Express', 60.00),
-    WishlistItemData("assets/6.jpeg", 'A Crown Of Wishes', 75.00),
-  ];
-
-  void _addItem() {
-    if (_currentIndex < _itemsToAdd.length) {
-      setState(() {
-        _wishlistItems.add(_itemsToAdd[_currentIndex]);
-        _currentIndex++;
-      });
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -54,14 +44,6 @@ class _WishlistState extends State<Wishlist> {
         title: const Text('Your Wishlist'),
         backgroundColor: Colors.lightGreen[300],
         elevation: 15,
-        actions: [
-          IconButton(
-              onPressed: () {}, icon: const Icon(Icons.shopping_cart, size: 35)),
-          IconButton(
-            icon: const Icon(Icons.add, size: 35),
-            onPressed: _addItem, // Adding a new item to the wishlist
-          ),
-        ],
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
